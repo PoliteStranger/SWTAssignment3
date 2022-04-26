ace7d41e33f79dc1ba381cc207b6c2abbe568ba2
Oliver Spange <oliver@spange.com>
2022-04-26 22:38:07 +0200

    Added buzzer to user interface and test of buzzer

--- Microwave.Classes/Controllers/UserInterface.cs
+++ Microwave.Classes/Controllers/UserInterface.cs
@@ -16,6 +16,7 @@ namespace Microwave.Classes.Controllers
         private ICookController myCooker;
         private ILight myLight;
         private IDisplay myDisplay;
+        private IBuzzer myBuzzer;
 
         private int powerLevel = 50;
         private int time = 1;
@@ -28,7 +29,8 @@ namespace Microwave.Classes.Controllers
             IDoor door,
             IDisplay display,
             ILight light,
-            ICookController cooker)
+            ICookController cooker,
+            IBuzzer buzzer)
         {
             powerButton.Pressed += new EventHandler(OnPowerPressed);
             timeButton.Pressed += new EventHandler(OnTimePressed);
@@ -40,6 +42,7 @@ namespace Microwave.Classes.Controllers
             myCooker = cooker;
             myLight = light;
             myDisplay = display;
+            myBuzzer = buzzer;
         }
 
         private void ResetValues()
@@ -150,8 +153,9 @@ namespace Microwave.Classes.Controllers
                     ResetValues();
                     myDisplay.Clear();
                     myLight.TurnOff();
-                    // Beep 3 times
                     myState = States.READY;
+                    // Beep 3 times
+                    myBuzzer.BurstBuzz(3);
                     break;
             }
         }
