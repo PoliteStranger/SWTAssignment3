150b0efc8de52d9b2c7f797df4d8e0836c56a54e
Oliver Spange <oliver@spange.com>
2022-04-26 22:09:30 +0200

    Created Buzzer and added its functionality with comments. Started on testing of Buzzer class !!NOT FINISHED!!

---
+++ Microwave.Test.Unit/BuzzerTest.cs
@@ -0,0 +1,34 @@
+﻿using Microwave.Classes.Boundary;
+using NUnit.Framework;
+
+namespace Microwave.Test.Unit
+{
+    [TestFixture]
+    public class BuzzerTest
+    {
+        private Buzzer uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Buzzer();
+        }
+
+        [TestCase(0)]
+        [TestCase(21)]
+        public void BurstBuzz_IllegalInputParameters(int times)
+        {
+            // We don't need an assert, as an exception would fail the test case
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => uut.BurstBuzz(times));
+
+        }
+
+        [TestCase(1)]
+        [TestCase(20)]
+        public void BurstBuzz_LegalInputParameters(int times)
+        {
+
+        }
+
+    }
+}
