4e26734e159ba09fabf5d39a04510c109cd95d22
Oliver Spange <oliver@spange.com>
2022-04-26 22:20:45 +0200

    Finished tests of buzzer class

--- Microwave.Test.Unit/BuzzerTest.cs
+++ Microwave.Test.Unit/BuzzerTest.cs
@@ -23,12 +23,25 @@ namespace Microwave.Test.Unit
 
         }
 
-        [TestCase(1)]
-        [TestCase(20)]
-        public void BurstBuzz_LegalInputParameters(int times)
+        [Test]
+        public void BurstBuzz_LegalInputParameters_1buzz()
         {
-
+            uut.BurstBuzz(1);
+            Assert.That(uut.LastBuzzAmount, Is.EqualTo(1));
         }
 
+        [Test, Timeout(800)] //timeout to ensure that the buzzes dont take too long
+        public void BurstBuzz_LegalInputParameters_3buzz()
+        {
+            uut.BurstBuzz(3);
+            Assert.That(uut.LastBuzzAmount, Is.EqualTo(3));
+        }
+
+        [Test, Timeout(4500)] //timeout to ensure that the buzzes dont take too long
+        public void BurstBuzz_LegalInputParameters_20buzz()
+        {
+            uut.BurstBuzz(20);
+            Assert.That(uut.LastBuzzAmount, Is.EqualTo(20));
+        }
     }
 }
